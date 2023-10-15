4a51aa23fdb715edd2062704862a2bd2bc295523
pruthvii09 <contact.pruthvirajauti@gmail.com>
2023-10-15 11:50:54 +0530

    chnages in pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,19 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom"
-import './App.css';
-import Layout from './Components/Layout';
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import Home from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Login from "./pages/Login/Login";
+import Signup from "./pages/Signup/Signup";
+import Home from "./pages/Home/Home";
+import Layout from "./Components/Layout";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Layout/>
+        <Layout />
         <Routes>
-        <Route path="/login" element={<Login/>} />
-        <Route path="/signup" element={<Signup/>} />
-        <Route path="/" element={<Home/>} />
-          
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/" element={<Home />} />
         </Routes>
       </Router>
     </div>
---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+function Home() {
+  return <div>hiiiiiiii</div>;
+}
+
+export default Home;
